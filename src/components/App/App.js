6da2598e45856c3040518bc654869f3be55b0859
OtomiVetra <otomivetra@mail.ru>
2024-07-13 01:48:00 +0300

    todo_with_JSON_placeholder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,28 @@
+import { useState, useEffect } from 'react';
+import styles from './App.module.css';
+
 export const App = () => {
-	return <div>test</div>;
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((response) => response.json())
+			.then((data) => setTodos(data))
+			.catch((error) => console.error('что то пошло не так:', error));
+	}, []);
+
+	return (
+		<div className={styles.app}>
+			<div className={styles.todoListContainer}>
+				<h1 className={styles.header}>TodoList</h1>
+				<ul className={styles.todoList}>
+					{todos.map((todo) => (
+						<li key={todo.id} className={styles.todoListItem}>
+							{todo.title}
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 };
